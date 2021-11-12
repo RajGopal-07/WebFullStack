9cea14e0942495ff901e3d4ba3b6c6c3263a9284
githubkola <raju916486@gmail.com>
2021-11-12 12:32:58 +0530

    react

---
+++ java/Assignment/Divisibleby7.java
@@ -0,0 +1,12 @@
+public class Divisibleby7 {
+    public static void main(String[] args){
+        int num = 70;
+        if(num % 7 == 0){
+            System.out.println(num +" this is Divisible by 7");
+        }
+        else{
+            System.out.println(num +" this is Not Divisible by 7");
+        }
+    }
+    
+}
